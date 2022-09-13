747740fd1bfe0a3318cff5e3f80a2fd9dc893573
Tyler Shakibai <tyler.shakibai@gmail.com>
2022-09-12 18:37:49 -0700

    Added contact bar

--- src/components/home.js
+++ src/components/home.js
@@ -2,8 +2,25 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
+        <div className='Main'>
             <img src='waimea-pic.png' alt='Waimea' className='Picture'/>
+            <div className='Info'>
+                <p className='Description'>
+                    I am currently a graduate student in the Department of Applied Mathematics at the University of Washington in Seattle, WA.
+                    My interests include dynamicals systems, numerical analysis, and data science.
+                </p>
+                <div className='Contacts'>
+                    <a href='mailto:tshak@uw.edu'>
+                        <img src='email-icon.png' alt='Email' className='Email'/>
+                    </a>
+                    <a href='https://github.com/TylerShakibai'>
+                        <img src='github-icon.png' alt='GitHub' className='GitHub'/>
+                    </a>
+                    <a href='https://github.com/TylerShakibai'>
+                        <img src='linkedin-icon.png' alt='LinkedIn' className='LinkedIn'/>
+                    </a>
+                </div>
+            </div>
         </div>
     );
   };
