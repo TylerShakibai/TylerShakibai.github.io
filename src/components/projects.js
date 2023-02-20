6310f2efe1c25d5387093d11b2af8c01fe0ed95a
Tyler Shakibai <tyler.shakibai@gmail.com>
2023-02-19 23:06:48 -0800

    Set up project section

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,10 +2,27 @@ import React from 'react';
 
 const Projects = () => {
     return (
-        <>
-            This is where projects go.
-        </>
+        <div className='Project-List'>
+            <article>
+                <div className='Project'>
+                    <div>
+                        <a href='https://github.com/TylerShakibai/TylerShakibai.github.io' className='Links'>This Website</a>
+                        <p>My personal website. Built using React, JavaScript, HTML, and CSS.</p>
+                    </div>
+                    <img src='favicon.ico' alt='Website icon' className='Project-Icon'/>
+                </div>
+            </article>
+            <article>
+                <div className='Project'>
+                    <div>
+                        <a href='https://github.com/TylerShakibai/Solar-Spectra-Model' className='Links'>Solar Spectra Model</a>
+                        <p>Convolutional Neural Network for Solar Spectra Regression.</p>
+                    </div>
+                    <img src='solar-spectra.png' alt='Solar spectra icon' className='Project-Icon'/>
+                </div>
+            </article>
+        </div>
     );
-  };
+};
 
 export default Projects;
